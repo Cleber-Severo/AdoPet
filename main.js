5c9dfe92f4258e082d9cbaab537e39d204d0280e
Cléber Severo <clebersevero1998@gmail.com>
2022-10-20 21:27:00 -0300

    adding the messagem and user-profile pages and referencing the headers anchor icons

--- main.js
+++ main.js
@@ -4,27 +4,30 @@ visibilityBtn1.addEventListener("click", toggleVisibility1)
 const visibilityBtn2 = document.getElementById("visibilityBtn2")
 visibilityBtn2.addEventListener("click", toggleVisibility2)
 
+
 const visibilityBtn3 = document.getElementById("visibilityBtn3")
 visibilityBtn3.addEventListener("click", toggleVisibility3)
+
+
 
 
 function toggleVisibility1() {
     const passwordInput1 = document.getElementById("cadastro__senha")
 
-    const type =  passwordInput1.getAttribute("type")=== "password" ? "text" : "password"
+    const type =  passwordInput1.getAttribute("type") === "password" ? "text" : "password"
     passwordInput1.setAttribute("type", type)
 }
 
 function toggleVisibility2() {
     const passwordInput2 = document.getElementById("cadastro__senhaConfirma")
 
-    const type =  passwordInput2.getAttribute("type")=== "password" ? "text" : "password"
+    const type =  passwordInput2.getAttribute("type") === "password" ? "text" : "password"
     passwordInput2.setAttribute("type", type)
 }
 
 function toggleVisibility3() {
-    const passwordInput3 = document.getElementById("senha")
+    const passwordInput3 = document.getElementById("senha_login")
 
-    const type =  passwordInput3.getAttribute("type")=== "password" ? "text" : "password"
-    passwordInput3.setAttribute("type", type)
+    const typeC =  passwordInput3.getAttribute("type") === "password" ? "text" : "password"
+    passwordInput3.setAttribute("type", typeC)
 }
